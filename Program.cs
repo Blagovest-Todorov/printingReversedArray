61648367099c937e6ffb600defaada4e483c77dd
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-11-18 12:10:20 +0200

    new change to explain indexing and number of elements and value of elements

--- Program.cs
+++ Program.cs
@@ -11,8 +11,8 @@ namespace _01.arrayExamplesTasks
         {
             Console.WriteLine("Reversed : ");
             int length = 12;
-            int[] arr = Enumerable.Range(2, length).ToArray(); //this is->Row (2,3,4,5,6,7,8,9,10,11,12,13) 
-                                                               //index:    (0,1,2,3,4,5,6,7, 8, 9,10,11 //for 12 elements
+            int[] arr = Enumerable.Range(2, length).ToArray(); //numbersValue (2,3,4,5,6,7,8,9,10,11,12,13) //12 elements here
+                                                               //index:       (0,1,2,3,4,5,6,7, 8, 9,10,11 //for 12 elements
 
             for (int i = length - 1; i >= 0; i--)
             {
