1a3866d547457736611b98714f7d7da4e336e4ab
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-11-18 12:08:03 +0200

    better explained enumeration of indexes and array elements!

--- Program.cs
+++ Program.cs
@@ -9,13 +9,12 @@ namespace _01.arrayExamplesTasks
     {
         static void Main(string[] args)
         {
-
             Console.WriteLine("Reversed : ");
             int length = 12;
-            int[] arr = Enumerable.Range( 2, length  ).ToArray(); // this is -> Row (9, 8, 7, 6, 5, 4, 3, 2, 1, 0) 
-                                                                     //index 0, 1, 2, ..9//for 10 items
+            int[] arr = Enumerable.Range(2, length).ToArray(); //this is->Row (2,3,4,5,6,7,8,9,10,11,12,13) 
+                                                               //index:    (0,1,2,3,4,5,6,7, 8, 9,10,11 //for 12 elements
 
-            for (int i = length -1 ; i >= 0; i --)
+            for (int i = length - 1; i >= 0; i--)
             {
                 int number = arr[i];
                 Console.WriteLine(number);
